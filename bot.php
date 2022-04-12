2975484258db83fdf15ac6e476d19cca7b3e8b87
iewilmaestro <83399918+iewilmaestro@users.noreply.github.com>
2022-04-12 23:00:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,6 @@
 <?php
 error_reporting(0);
+require "cfg.php";
 
 const
 b = "\033[1;34m",
@@ -51,7 +52,7 @@ function Run($url, $head = 0, $post = 0){
 	}
 }
 $line = str_repeat('~',50)."\n";
-$h[] = "cookie: _ga=GA1.1.2135199477.1649241076; bitmedia_fid=eyJmaWQiOiJhM2Q0MjA1OTA1MDc5MjEyZDUzNGFkMjJmMDQ3MDYwYiIsImZpZG5vdWEiOiJhOGRjZGFmMDBiYTY4ZjllZTliNzBjZGJiNmVkYTU1NSJ9; __gads=ID=ee6a559115b45305-220c69a1edd10050:T=1649754583:RT=1649754583:S=ALNI_MbRXsb86Y-W1440f4jZYpx-eJGG4Q; _coinzilla_fp_=%7B%22backed%22%3A%5B%7B%22cap%22%3A10%2C%22lastCall%22%3A1649772268183%7D%5D%7D; ci_session=a05394414077fe445f071ec0c6104250ef65d31e; ads_300x250-0=seen; ads_300x250-1=seen; cf_clearance=PdVi0V0DbgCACX1sR4mgnjavZ2fIHAVrcQVVexphmRE-1649777630-0-150; _data_cpc=2-1-1649779415_286-1-1649781332_360-1-1649781236_361-1-1649779326_411-1-1649779861; csrf_cookie_name=884165f62636570aaa786118496606ba; ads_468x600-0=seen; ads_728x90-0=seen; _ga_58PCYERW4T=GS1.1.1649770866.3.1.1649777757.0; _ga_HF8YJ4L2V3=GS1.1.1649774816.4.1.1649777757.0";
+$h[] = "cookie: ".$cookie;
 $h[] = "user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36";
 
 $res = Run('https://bifaucet.com/dashboard', $h);
